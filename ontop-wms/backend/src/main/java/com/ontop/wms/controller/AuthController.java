7c2d196d37cd81fd0daa0a9775e7b904104f8e66
phantrantien5 <phantrantien5@gmail.com>
2026-04-07 18:40:45 +0000

    n84

--- ontop-wms/backend/src/main/java/com/ontop/wms/controller/AuthController.java
+++ ontop-wms/backend/src/main/java/com/ontop/wms/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.ontop.wms.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,14 +11,16 @@ import com.ontop.wms.dto.AuthResponse;
 import com.ontop.wms.dto.LoginRequest;
 import com.ontop.wms.service.AuthService;
 
-import lombok.RequiredArgsConstructor;
-
 @RestController
 @RequestMapping("/auth")
-@RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
