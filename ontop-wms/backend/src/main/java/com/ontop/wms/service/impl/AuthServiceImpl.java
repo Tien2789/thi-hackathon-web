42cffb296c99a402af428da1c87334edc68a091c
phantrantien5 <phantrantien5@gmail.com>
2026-04-08 16:54:27 +0000

    n

--- ontop-wms/backend/src/main/java/com/ontop/wms/service/impl/AuthServiceImpl.java
+++ ontop-wms/backend/src/main/java/com/ontop/wms/service/impl/AuthServiceImpl.java
@@ -1,9 +1,10 @@
-
 package com.ontop.wms.service.impl;
 
 import com.ontop.wms.dto.AuthResponse;
 import com.ontop.wms.dto.LoginRequest;
+import com.ontop.wms.entity.Role;
 import com.ontop.wms.entity.User;
+import com.ontop.wms.entity.Warehouse;
 import com.ontop.wms.repository.UserRepository;
 import com.ontop.wms.service.AuthService;
 import com.ontop.wms.service.JwtService;
@@ -11,6 +12,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class AuthServiceImpl implements AuthService {
@@ -39,6 +43,12 @@ public class AuthServiceImpl implements AuthService {
 
         String jwt = jwtService.generateToken(user);
 
-        return new AuthResponse(jwt);
+        String roleName = user.getRoles().stream().map(Role::getName).findFirst().orElse(null);
+
+        Optional<Warehouse> warehouseOptional = user.getWarehouses().stream().findFirst();
+        Long warehouseId = warehouseOptional.map(warehouse -> Long.valueOf(warehouse.getId())).orElse(null);
+        String warehouseName = warehouseOptional.map(Warehouse::getName).orElse(null);
+
+        return new AuthResponse(jwt, user.getUsername(), roleName, warehouseId, warehouseName);
     }
 }
